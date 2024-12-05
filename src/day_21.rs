10f35293c17a97469f088844faf7416b4689590f
Waridley <Waridley64@gmail.com>
2024-12-05 17:10:45 -0600

    Add remaining day files

---
+++ src/day_21.rs
@@ -0,0 +1,12 @@
+use std::fmt::Display;
+use anyhow::Result;
+
+crate::decl_tests! {}
+
+fn eval_pt_1(input: &str) -> Result<impl Display> {
+	Ok("todo")
+}
+
+fn eval_pt_2(input: &str) -> Result<impl Display> {
+	Ok("todo")
+}
